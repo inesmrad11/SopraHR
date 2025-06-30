1bffb35fd11465855fadb38c87bd2c750665ef7a
inesmrad11 <147980758+inesmrad11@users.noreply.github.com>
2025-06-30 14:53:23 +0100

    Initial commit: added frontend and backend structure

---
+++ avance-salaire-backend/avance-salaire-backend/src/main/java/com/soprahr/avancesalairebackend/AvanceSalaireBackendApplication.java
@@ -0,0 +1,13 @@
+package com.soprahr.avancesalairebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvanceSalaireBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvanceSalaireBackendApplication.class, args);
+	}
+
+}
---
+++ avance-salaire-backend/avance-salaire-backend/src/test/java/com/soprahr/avancesalairebackend/AvanceSalaireBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soprahr.avancesalairebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvanceSalaireBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
